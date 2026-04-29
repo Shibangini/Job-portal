93caaaacf295bc4f2e615770062948633f90b4c9
shibangini_kar <shibangini25@gmail.com>
2026-04-30 02:48:34 +0530

    fix: persist auth and update applicant counts

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
 import authReducer from "./authSlice";
 import jobReducer from "./jobSlice";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+};
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    job: jobReducer,
+});
+const authPersistedReducer = persistReducer(persistConfig, rootReducer);
+
 
 const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        job: jobReducer,
-    },
+    reducer: authPersistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
 
 export default store;
