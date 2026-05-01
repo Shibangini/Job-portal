1827773d1d763158f5491ede38530ac3f8f35974
shibangini_kar <shibangini25@gmail.com>
2026-05-02 02:18:38 +0530

    fix: new company post, company table, company state, company search filter, and white screen issues

--- frontend/src/components/admin/companiesTable.jsx
+++ frontend/src/components/admin/companiesTable.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Table, TableCaption, TableHead, TableHeader, TableRow, TableBody, TableCell } from "../ui/table"
 import { Avatar, AvatarImage } from '../ui/avatar'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 import { MoreHorizontal, Edit2 } from 'lucide-react'
+import { useSelector } from 'react-redux'
 
 const companiesTable = () => {
+    const { companies = [], searchCompanyByText = "" } = useSelector((store) => store.company) || {};
+    const filteredCompanies = companies.filter((company) => {
+        if (!searchCompanyByText) {
+            return true;
+        }
+        return company?.name?.toLowerCase().includes(searchCompanyByText.toLowerCase());
+    });
     return (
         <Table>
             <TableCaption>A list of your recent registered companies</TableCaption>
@@ -13,34 +21,44 @@ const companiesTable = () => {
                     <TableHead>Logo</TableHead>
                     <TableHead>Name</TableHead>
                     <TableHead>Date</TableHead>
-                    <TableHead className="tesxt-right">Action</TableHead>
+                    <TableHead className="text-right">Action</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
-                <TableRow>
-                    <TableCell>
-                        <Avatar className='h-8 w-8'>
-                            <AvatarImage src="https://www.siegelgale.com/app/uploads/2021/10/SGCOM_Blog_211018.png" />
-                        </Avatar>
-                    </TableCell>
-                    <TableCell>Company Name</TableCell>
-                    <TableCell>30-04-2026</TableCell>
-                    <TableCell className="text-right cursor-pointer">
-                        <Popover>
-                            <PopoverTrigger>
-                                <MoreHorizontal />
-                            </PopoverTrigger>
-                            <PopoverContent className='w-32'>
-                                <div className='flex items-center gap-2 w-fit cursor-pointer'>
-                                    <Edit2 className='w-4' />
-                                    <span>Edit</span>
-                                </div>
-                            </PopoverContent>
-                        </Popover>
-                    </TableCell>
-                </TableRow>
+                {filteredCompanies.length <= 0 ? (
+                    <TableRow>
+                        <TableCell colSpan={4} className="text-center text-muted-foreground">
+                            No companies found
+                        </TableCell>
+                    </TableRow>
+                ) : (
+                    filteredCompanies.map((company) => (
+                        <TableRow key={company._id}>
+                            <TableCell>
+                                <Avatar className='h-8 w-8'>
+                                    <AvatarImage src={company.logo || "https://www.freepnglogos.com/uploads/company-logo-png/company-logo-transparent-png-19.png"} />
+                                </Avatar>
+                            </TableCell>
+                            <TableCell>{company.name}</TableCell>
+                            <TableCell>{company.createdAt?.split("T")[0]}</TableCell>
+                            <TableCell className="text-right cursor-pointer">
+                                <Popover>
+                                    <PopoverTrigger>
+                                        <MoreHorizontal />
+                                    </PopoverTrigger>
+                                    <PopoverContent className='w-32'>
+                                        <div className='flex items-center gap-2 w-fit cursor-pointer'>
+                                            <Edit2 className='w-4' />
+                                            <span>Edit</span>
+                                        </div>
+                                    </PopoverContent>
+                                </Popover>
+                            </TableCell>
+                        </TableRow>
+                    ))
+                )}
             </TableBody>
-        </Table>
+        </Table >
     )
 }
 
